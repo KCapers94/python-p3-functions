d1d1d40c8617a806f112dbd12b2e630f357a5bc5
Keith Capers <K.Capers94@gmail.com>
2024-09-19 20:22:33 -0400

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return(num1+num2)
 
 def halve(number):
-    pass
+   if not isinstance(number,(int,float)):
+        return None
+   return number / 2
+
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+# my_function_return_value = my_function(1)
+# print(my_function_return_value)
+
+
